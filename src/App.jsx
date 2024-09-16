353dcd834b91e0f3e08efe27e9dc6ca0f05bd902
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-16 14:46:08 -0300

    adicionando menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ function App() {
 
   return (
     <>
-      
+      <header id="top">
+          <h2>ICR | Portfolio</h2>
+          <nav>
+              <a href="#home"><h3>Home</h3></a>
+              <a href="#about-me"><h3>About Me</h3></a>
+              <a href="#my-skills"><h3>My Skills</h3></a>
+              <a href="#contact"><h3>Contact</h3></a>
+          </nav>
+      </header>
     </>
   )
 }
